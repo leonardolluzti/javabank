32082bc80e0e32569b5d5d5f57399c0fabac779a
Leonardo Lopes da Luz <leonardolluzti@gmail.com>
2021-04-08 21:15:26 -0300

    Versão com Menu

--- src/ContaExemplo.java
+++ src/ContaExemplo.java
@@ -1,13 +1,14 @@
+import java.util.Scanner;
 
 public class ContaExemplo {
 
 	public static void main(String[] args) {
-		// criação de conta corrente!
-		ContaCorrente cc = new ContaCorrente();
+		
+		// criação de conta corrente!	
 		Pessoa pessoa = new Pessoa();
 		pessoa.setNome("Leonardo");
 		pessoa.setCpf("123.456.789-10");
-		
+		ContaCorrente cc = new ContaCorrente();
 		cc.abrirConta();
 		cc.setPessoa(pessoa);
 		cc.setNumero(9876);
@@ -29,33 +30,173 @@ public class ContaExemplo {
 		cp.setAgencia(1304);
 		cp.setSaldo(100.00);
 		cp.setLimite(cp.getSaldo());
-		cp.exibirDados();
-		cp.exibirSaldo();
+// Menu
+		Scanner menu = new Scanner(System.in);
 		System.out.println("=======================================================");
-		// operações em conta corrente
-		cc.exibirDados();
-		cc.sacar(10);
-		cc.exibirSaldo();
+		System.out.println("Bem Vindo ao JavaBank, o que você gostaria de fazer?");
+		System.out.println("1 - Verificar dados da conta");
+		System.out.println("2 - Ver saldo");
+		System.out.println("3 - Fazer Depósito");
+		System.out.println("4 - Fazer Saque");
+		System.out.println("5 - Fazer Transferência");
+		System.out.println("6 - Sair");
 		System.out.println("=======================================================");
-		cc.exibirDados();
-		cc.exibirSaldo();
-		cc.depositar(10);
-		cc.exibirSaldo();
-		System.out.println("=======================================================");
-		cc.exibirSaldo();
-		cc.transferir(cp, 10);
-		cc.exibirSaldo();
-		cp.exibirSaldo();
-		// operações em conta poupança
-		cp.exibirDados();
-		cp.exibirSaldo();
-		cp.sacar(12);
-		cp.exibirSaldo();
-		System.out.println("=======================================================");
-		cp.exibirDados();
-		cp.exibirSaldo();
-		cp.depositar(10);
-		cp.exibirSaldo();
+		System.out.print("Opção: ");
+		int op = menu.nextInt();
+		
+		switch (op) {
+			//Verificar dados da conta
+			case 1: 
+				System.out.println("=======================================================");
+				System.out.println("Qual seu tipo de conta? ");
+				System.out.println("1 - Conta Corrente");
+				System.out.println("2 - Conta Poupança");
+				System.out.print("Opção: ");
+				int tpconta = menu.nextInt();
+				if (tpconta==1){
+					System.out.println("=======================================================");
+					cc.exibirDados();
+					System.out.println("=======================================================");
+				}
+				else if (tpconta==2){
+					System.out.println("=======================================================");
+					cp.exibirDados();
+					System.out.println("=======================================================");
+				}
+				else{
+					System.out.println("Opção inválida!");
+				}				
+				break;
+			//Ver saldo
+			case 2: 
+				System.out.println("=======================================================");
+				System.out.println("Qual seu tipo de conta? ");
+				System.out.println("1 - Conta Corrente");
+				System.out.println("2 - Conta Poupança");
+				System.out.print("Opção: ");
+				int tpconta2 = menu.nextInt();
+				if (tpconta2==1){
+					System.out.println("=======================================================");
+					cc.exibirSaldo();
+					System.out.println("=======================================================");
+				}
+				else if (tpconta2==2){
+					System.out.println("=======================================================");
+					cp.exibirSaldo();
+					System.out.println("=======================================================");
+				}
+				else{
+					System.out.println("Opção inválida!");
+				}
+				break;
+			//Fazer Depósito
+			case 3: 
+				System.out.println("=======================================================");
+				System.out.println("Qual seu tipo de conta? ");
+				System.out.println("1 - Conta Corrente");
+				System.out.println("2 - Conta Poupança");
+				System.out.print("Opção: ");
+				int tpconta3 = menu.nextInt();
+				if (tpconta3==1){
+					System.out.println("=======================================================");
+					System.out.print("Digite o Valor que Deseja depositar: R$");
+					double valor1 = menu.nextDouble();
+					System.out.println("=======================================================");
+					cc.exibirDados();
+					cc.exibirSaldo();
+					cc.depositar(valor1);
+					cc.exibirSaldo();
+					System.out.println("=======================================================");
+				}
+				else if (tpconta3==2){
+					System.out.println("=======================================================");
+					System.out.print("Digite o Valor que Deseja depositar: R$");
+					double valor2 = menu.nextDouble();
+					System.out.println("=======================================================");
+					cp.exibirDados();
+					cp.exibirSaldo();
+					cp.depositar(valor2);
+					cp.exibirSaldo();
+					System.out.println("=======================================================");
+					
+				}
+				else{
+					System.out.println("Opção inválida!");
+				}
+				break;
+			//Fazer Saque
+			case 4: 
+				System.out.println("=======================================================");
+				System.out.println("Qual seu tipo de conta? ");
+				System.out.println("1 - Conta Corrente");
+				System.out.println("2 - Conta Poupança");
+				System.out.print("Opção: ");
+				int tpconta4 = menu.nextInt();
+				if (tpconta4==1){
+					System.out.print("Digite o Valor que Deseja sacar: R$");
+					double valor3 = menu.nextDouble();
+					System.out.println("=======================================================");
+					cc.exibirDados();
+					cc.exibirSaldo();
+					cc.sacar(valor3);
+					cc.exibirSaldo();
+					System.out.println("=======================================================");
+				}
+				else if (tpconta4==2){
+					System.out.print("Digite o Valor que Deseja sacar: R$");
+					double valor4 = menu.nextDouble();
+					System.out.println("=======================================================");
+					cp.exibirDados();
+					cp.exibirSaldo();
+					cp.sacar(valor4);
+					cp.exibirSaldo();
+					System.out.println("=======================================================");
+					
+				}
+				else{
+					System.out.println("Opção inválida!");
+				}
+				break;
+			//Fazer Transferência
+			case 5: 
+				System.out.println("=======================================================");
+				System.out.println("Qual seu tipo de conta? ");
+				System.out.println("1 - Conta Corrente");
+				System.out.println("2 - Conta Poupança");
+				System.out.print("Opção: ");
+				int tpconta5 = menu.nextInt();
+				if (tpconta5==1){
+					System.out.print("Digite o Valor que Deseja Transferir: R$");
+					double valor5 = menu.nextDouble();
+					System.out.println("=======================================================");
+					cc.exibirSaldo();
+					cc.transferir(cp, valor5);
+					cc.exibirSaldo();
+					cp.exibirSaldo();
+					System.out.println("=======================================================");
+				}
+				else if (tpconta5==2){
+					System.out.print("Digite o Valor que Deseja Transferir: R$");
+					double valor6 = menu.nextDouble();
+					System.out.println("=======================================================");
+					cp.exibirSaldo();
+					cp.transferir(cc, valor6);
+					cp.exibirSaldo();
+					cc.exibirSaldo();
+					System.out.println("=======================================================");
+					
+				}
+				else{
+					System.out.println("Opção inválida!");
+				}				
+				break;
+			//Sair do Sistema
+			case 6: 
+				System.out.println("Até mais!");
+				break;
+			}
+		menu.close();		
+		System.out.println("Fim do Programa!");
 	}
 
 }
